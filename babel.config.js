e689862a2b04cecea8d3375a2bf0c99922bcf03f
Sel <selcuk.80@outlook.de>
2025-08-09 23:49:58 +0200

    DashboardView
    Name DashboardView

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,3 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ]
 }
-
-new webpack.DefinePlugin({
-  __VUE_OPTIONS_API__: JSON.stringify(true),
-  __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
-  __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false)
-})
